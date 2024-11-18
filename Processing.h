d6e5cc885c72e21cba9dcf6e71b2ef978f7097e4
codestar <glegat@gaming.tech>
2024-11-18 12:49:49 +0100

    Update Processing.h

--- Processing.h
+++ Processing.h
@@ -5,4 +5,5 @@ namespace processing
 {
 	void convertToGrey(Image& image);
 	void convertToBinary(Image& image, const uint8& threshold = 122);
+	void medianFilter(Image& image, const int& filterSize = 3);
 }
